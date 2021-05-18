276d4e7d1a8ae3f731fbf7e46315f7c2bfca3e37
hongson97 <58877315+hongson97@users.noreply.github.com>
2021-05-18 22:21:26 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,8 +2,10 @@
 wget https://github.com/exiftool/exiftool/archive/refs/tags/12.23.zip
 sudo apt install unzip
 sudo apt install python3
+sudo apt install python3-pip
 sudo apt-get install -y libarchive-zip-perl
 unzip 12.23.zip
 rm 12.23.zip
+
 pip3 install -r requirements.txt
 echo "HCMUS-CTF{CVE_22204_1s_v3ry_1nt3r3st1ng}" > /tmp/flag.txt
